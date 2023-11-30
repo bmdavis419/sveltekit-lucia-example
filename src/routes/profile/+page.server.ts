60e36188c1b9025a8e4d27c07b921acfae923c04
Benjamin Davis <davis.benjamin41902@gmail.com>
2023-11-29 21:10:38 -0500

    types

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -4,7 +4,7 @@ export const load = async ({ locals }) => {
 	const session = await locals.auth.validate();
 	if (!session) throw redirect(302, '/signup');
 	return {
-		userId: session.user.userId,
-		email: session.user.email
+		userId: session.user.userId as string,
+		email: session.user.email as string
 	};
 };
